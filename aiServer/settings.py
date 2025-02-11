4ea5e6ebc53a0cdaf9db192a45ceddd4af27b28f
임태연 <limtaeyeon@imtaeyeon-ui-MacBookAir.local>
2025-02-11 20:55:38 +0900

    wllowed setting

--- aiServer/settings.py
+++ aiServer/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-3zll&q3a*lt1t=9ajy^#fhkh2!&^os$x52_g*d(d+ndul-$l@z
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
